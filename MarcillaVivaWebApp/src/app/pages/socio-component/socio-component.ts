ac3e61d0c5d0396465211099740e179b561a2f25
ANUAR-MSI\Adrián <adrian@nakama.es>
2025-12-04 18:58:32 +0100

    04/12/2025

--- MarcillaVivaWebApp/src/app/pages/socio-component/socio-component.ts
+++ MarcillaVivaWebApp/src/app/pages/socio-component/socio-component.ts
@@ -1,11 +1,41 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-socio-component',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './socio-component.html',
   styleUrl: './socio-component.css',
 })
 export class SocioComponent {
 
+  socioForm: FormGroup;
+
+  formasPago = ['Bizum', 'Transferencia', 'PayPal'];
+
+  constructor(private fb: FormBuilder) {
+    this.socioForm = this.fb.group({
+      nombre: ['', Validators.required],
+      apellidos: ['', Validators.required],
+      dni: ['', Validators.required],
+      domicilio: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      telefono: ['', Validators.required],
+      formaPago: ['', Validators.required],
+      aceptaPoliticas: [false, Validators.requiredTrue]
+    });
+  }
+
+  enviar() {
+    if (this.socioForm.valid) {
+      console.log('Datos enviados:', this.socioForm.value);
+    } else {
+      this.socioForm.markAllAsTouched();
+    }
+  }
+
+  redirigirPago() {
+  window.location.href = "https://buy.stripe.com/test_abc123";
+}
 }
